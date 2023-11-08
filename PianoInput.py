e1413d1348f428a60a7f03e3e6495e477fbbf474
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2023-11-08 23:11:21 +0100

    first commit

---
+++ PianoInput.py
@@ -0,0 +1,3 @@
+class PianoInput:
+    def test_function(self):
+        print("Piano input test function called.")
---
+++ main.py
@@ -0,0 +1,8 @@
+from PianoInput import PianoInput
+
+def main():
+    piano_input = PianoInput()
+    piano_input.test_function()
+
+if __name__ == "__main__":
+    main()
