eb18ca1e7d313c17b183241de30bae1a59eae17c
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2023-11-09 19:26:08 +0100

    async callback api for get midi note

--- PianoInput.py
+++ PianoInput.py
@@ -15,12 +15,10 @@ class PianoInput:
     def __init__(self, input_device=None):
         if input_device is None:
             input_device = PianoInput.get_first_working_port()
-        PianoInput.set_input_device(input_device)
-        # Print available input ports
         print("Available MIDI input devices:")
         print(mido.get_input_names())
+        PianoInput.set_input_device(input_device)
 
-    @staticmethod
     def get_first_working_port():
         for port_name in mido.get_input_names():
             try:
@@ -30,41 +28,20 @@ class PianoInput:
                 pass
         return None
 
-    async def get_midi_out(self):
+    async def get_midi_out(self, callback):
         try:
-            with mido.open_input(PianoInput.get_input_device(), callback=note_handler) as port:
+            with mido.open_input(PianoInput.get_input_device(), callback=callback) as port:
                 print("Using {}".format(port))
                 print('MIDI port opened:', port)
                 while True:
                     message = port.receive()
                     if message.type == 'note_on':
-                        return message.note
+                        callback(message.note)
                     elif message.type == 'note_off':
-                        return message.note
+                        callback(message.note)
         except ValueError:
             print(f"Could not find {PianoInput.get_input_device()}. Available ports are:")
             print(mido.get_input_names())
-
-    """
-    def test_function(self):
-        print("Piano input test function called.")
-        print("Available MIDI input devices:")
-        print(mido.get_input_names())
-        # Set up MIDI input
-        try:
-            with mido.open_input(PianoInput.get_input_device(), callback=note_handler) as port:
-                print("Using {}".format(port))
-                print('MIDI port opened:', port)
-                while True:
-                    message = port.receive()
-                    if message.type == 'note_on':
-                        print('Note on:', message.note)
-                    elif message.type == 'note_off':
-                        print('Note off:', message.note)
-        except ValueError:
-            print(f"Could not find {PianoInput.get_input_device()}. Available ports are:")
-            print(mido.get_input_names())
-    """
 
 def note_handler(note: mido.Message) -> None:
     """
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import asyncio
+import mido
 from PianoInput import PianoInput
 
 async def main():
     piano_input = PianoInput()
-    print(await piano_input.get_midi_out())
+    await piano_input.get_midi_out(callback=handle_message)
+
+def handle_message(note):
+    print(f"Received message: {note}")
 
 if __name__ == '__main__':
     asyncio.run(main())
