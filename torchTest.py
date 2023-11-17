3e9d5cefe1b9f6e2470d5eb0f93a108585655636
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2023-11-17 11:41:34 +0100

    Test cuda usage

--- torchTest.py
+++ torchTest.py
@@ -1,17 +1,23 @@
 import torch
 
+# Print found devices
 devices = [d for d in range(torch.cuda.device_count())] 
 device_names  = [torch.cuda.get_device_name(d) for d in devices]
 print(device_names)
 
+# Cuda device to use .to(device)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 
-x = torch.empty(1)
+
+x = torch.empty(1).to(device)
 print("empty(1): ", x)
 
-x = torch.empty(3)
+x = torch.empty(3).to(device)
 print("empty(3): ", x)
 
-x = torch.empty(2, 3)
+x = torch.empty(2, 3).to(device)
+
 print("empty(2, 3): ", x)
-print("x Size: ", x.size())
+print("x size: ", x.size())
 print("x Shape: ", x.shape)
