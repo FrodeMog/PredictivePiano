a51e3874fbf8d449e9341dc4a7e926f9c285657b
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2023-11-17 11:23:57 +0100

    Example CUDA gpu found + testet

---
+++ torchTest.py
@@ -0,0 +1,15 @@
+import torch
+
+devices = [d for d in range(torch.cuda.device_count())] 
+device_names  = [torch.cuda.get_device_name(d) for d in devices]
+print(device_names)
+
+
+x = torch.empty(1)
+print("empty(1): ", x)
+
+x = torch.empty(3)
+print("empty(3): ", x)
+
+x = torch.empty(2, 3)
+print("empty(2, 3): ", x)
