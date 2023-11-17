7339b5c1480e70a834dd281707c30026e50a5e4f
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2023-11-17 15:41:58 +0100

    MNIST numbers example

---
+++ firstNeuralnet.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+# Print found devices
+devices = [d for d in range(torch.cuda.device_count())] 
+device_names  = [torch.cuda.get_device_name(d) for d in devices]
+print(device_names)
+
+# Cuda device to use .to(device)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+#hyperparameters
+input_size = 784 # 28x28
+hidden_size = 100
+num_classes = 10
+num_epochs = 10
+batch_size = 100
+learning_rate = 0.001
+
+#MNIST
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
+
+#Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+examples = iter(test_loader)
+example_data, example_targets = next(examples)
+
+for i in range(6):
+    plt.subplot(2,3,i+1)
+    plt.imshow(example_data[i][0], cmap='gray')
+#plt.show()
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size) 
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out) 
+        # no activation and no softmax at the end
+        return out
+    
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+
+#loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+#Train the model
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):  
+        # origin shape: [100, 1, 28, 28]
+        # resized: [100, 784]
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+        
+        #forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        
+        #backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if (i+1) % 100 == 0:
+            print(f'epoch {epoch+1}/{num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}')
+
+#Test the model
+with torch.no_grad():
+    n_correct = 0
+    n_samples = len(test_loader.dataset)
+
+    for images, labels in test_loader:
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+
+        #max returns (output_value, index)
+        _, predictions = torch.max(outputs, 1)
+        n_correct += (predictions == labels).sum().item()
+
+    acc = n_correct / n_samples
+    print(f"accuracy of the network on the {n_samples} test images: {100*acc} %")
