557308cfa71eb247655b3b38c5e36de681341420
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2023-11-17 12:32:51 +0100

    linear regression example

--- torchTest.py
+++ torchTest.py
@@ -9,7 +9,7 @@ print(device_names)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-
+"""
 x = torch.empty(1).to(device)
 print("empty(1): ", x)
 
@@ -18,6 +18,57 @@ print("empty(3): ", x)
 
 x = torch.empty(2, 3).to(device)
 
+x = torch.randn(2, 3).to(device) # First create on cpu then move to device (slower)
+print("rnd x : ", x)
+x = torch.randn(2, 3, device=device) # Created directly on device (faster)
+print("rnd x : ", x)
+
 print("empty(2, 3): ", x)
 print("x size: ", x.size())
 print("x Shape: ", x.shape)
+"""
+
+#linear regression example
+
+X = torch.tensor([1, 2, 3, 4, 5, 6, 7, 8 ], dtype=torch.float32, device = device)
+Y = torch.tensor([2, 4, 6, 8, 10, 12, 14, 16], dtype=torch.float32, device = device)
+
+W = torch.tensor(0.0, dtype=torch.float32, requires_grad=True, device = device)
+
+#model output
+def forward(X):
+    return X * W
+
+#loss = MSE
+def loss(y, y_pred):
+    return ((y_pred - y)**2).mean()
+
+X_test = 5.0
+
+print(f"Prediction before training: f({X_test}) = {forward(X_test).item():.3f}")
+
+#Training
+learning_rate = 0.001
+n_epochs = 200
+
+for epoch in range(n_epochs):
+    #prediction = forward pass
+    y_pred = forward(X)
+
+    #loss
+    l = loss(Y, y_pred)
+
+    #gradients = backward pass
+    l.backward() #dl/dw
+
+    #update weights
+    with torch.no_grad():
+        W -= learning_rate * W.grad
+
+    #zero gradients
+    W.grad.zero_()
+
+    if epoch % 10 == 0:
+        print(f"epoch {epoch+1}: w = {W:.3f}, loss = {l:.8f}")
+
+print(f"Prediction after training: f({X_test}) = {forward(X_test).item():.3f}")
