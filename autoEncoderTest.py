67320d09ec9f2811018bca5139b5c68d5be15b38
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2023-11-18 18:35:02 +0100

    working but bad sounding

--- autoEncoderTest.py
+++ autoEncoderTest.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from torch import nn
-
+from midiDataHandler import MidiDataHandler
 
 # Load the data
 data = pd.read_csv('test.csv')
@@ -12,32 +12,25 @@ data = pd.read_csv('test.csv')
 data['feature'] = data['feature'].apply(eval)
 data['label'] = data['label'].apply(eval)
 
-# Flatten the data
-data_flat = [item for sublist in data['feature'].tolist() for item in sublist]
-data_flat += [item for sublist in data['label'].tolist() for item in sublist]
-
 # Scale the data to the range [0, 1]
 scaler = MinMaxScaler()
-data_flat = scaler.fit_transform(np.array(data_flat).reshape(-1, 1))
 
-# Convert the data to PyTorch tensors
-data_tensor = torch.tensor(data_flat, dtype=torch.float32)
-
-# Reshape the data to match the input shape of the model
-data_tensor = data_tensor.view(-1, 10)
+# Convert the features and labels to PyTorch tensors
+feature_tensor = torch.tensor(scaler.fit_transform(np.array(data['feature'].tolist())), dtype=torch.float32)
+label_tensor = torch.tensor(scaler.transform(np.array(data['label'].tolist())), dtype=torch.float32)
 
 class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(10, 5),
+            nn.Linear(5, 5),
             nn.ReLU(),
             nn.Linear(5, 2)
         )
         self.decoder = nn.Sequential(
             nn.Linear(2, 5),
             nn.ReLU(),
-            nn.Linear(5, 10),
+            nn.Linear(5, 5),
             nn.Sigmoid()  # because we scaled our data to be in the range [0, 1]
         )
 
@@ -45,7 +38,7 @@ class Autoencoder(nn.Module):
         x = self.encoder(x)
         x = self.decoder(x)
         return x
-    
+
 from torch.optim import Adam
 
 # Initialize the autoencoder and the optimizer
@@ -53,10 +46,10 @@ model = Autoencoder()
 optimizer = Adam(model.parameters(), lr=0.0001)
 
 # Train the autoencoder
-for epoch in range(1000):
+for epoch in range(10000):
     # Forward pass
-    outputs = model(data_tensor)
-    loss = nn.MSELoss()(outputs, data_tensor)
+    outputs = model(feature_tensor)
+    loss = nn.MSELoss()(outputs, label_tensor)
 
     # Backward pass and optimize
     optimizer.zero_grad()
@@ -64,22 +57,19 @@ for epoch in range(1000):
     optimizer.step()
 
     if (epoch+1) % 100 == 0:
-        print(f'Epoch {epoch+1}/{1000}, Loss: {loss.item()}')
+        print(f'Epoch {epoch+1}/{10000}, Loss: {loss.item()}')
 
 # Define the input array
 input_array = np.array([69, 60, 65, 64, 69])
 
-# Pad the input array with zeros until it has 10 elements
-input_array = np.pad(input_array, (0, 10 - len(input_array)), 'constant')
+# Reshape the input to have 5 features
+input_array = input_array.reshape(1, -1)
 
 # Scale the input to the range [0, 1] using the same scaler used for training
-input_scaled = scaler.transform(input_array.reshape(-1, 1))
+input_scaled = scaler.transform(input_array)
 
 # Convert the input to a PyTorch tensor
 input_tensor = torch.tensor(input_scaled, dtype=torch.float32)
-
-# Reshape the input to match the input shape of the model
-input_tensor = input_tensor.view(-1, 10)
 
 # Pass the input through the model
 output = model(input_tensor)
@@ -93,7 +83,9 @@ output_original = scaler.inverse_transform(output_array)
 # Clip the values to the MIDI range
 output_clipped = np.clip(output_original, 0, 127)
 
-# Round the values to the nearest integer
-output_rounded = np.round(output_clipped)
+# Round the values to the nearest integer and convert to int
+output_rounded = np.round(output_clipped).astype(int)
 
-print(output_rounded)
+handler = MidiDataHandler()
+print(handler.midi_notes_to_piano(input_array[0]))
+print(handler.midi_notes_to_piano(output_rounded[0]))
