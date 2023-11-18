f4be874b5467905ed246ff0a3201cc6f8ea98c86
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2023-11-18 19:25:32 +0100

    make output.midi to test result

--- autoEncoderTest.py
+++ autoEncoderTest.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from torch import nn
 from midiDataHandler import MidiDataHandler
+import mido
 
 # Load the data
 data = pd.read_csv('test.csv')
@@ -44,9 +45,10 @@ from torch.optim import Adam
 # Initialize the autoencoder and the optimizer
 model = Autoencoder()
 optimizer = Adam(model.parameters(), lr=0.0001)
+epochCount = 1000
 
 # Train the autoencoder
-for epoch in range(10000):
+for epoch in range(epochCount):
     # Forward pass
     outputs = model(feature_tensor)
     loss = nn.MSELoss()(outputs, label_tensor)
@@ -57,35 +59,72 @@ for epoch in range(10000):
     optimizer.step()
 
     if (epoch+1) % 100 == 0:
-        print(f'Epoch {epoch+1}/{10000}, Loss: {loss.item()}')
+        print(f'Epoch {epoch+1}/{epochCount}, Loss: {loss.item()}')
+
+def process_input(input_array):
+    # Reshape the input to have 5 features
+    input_array = input_array.reshape(1, -1)
+
+    # Scale the input to the range [0, 1] using the same scaler used for training
+    input_scaled = scaler.transform(input_array)
+
+    # Convert the input to a PyTorch tensor
+    input_tensor = torch.tensor(input_scaled, dtype=torch.float32)
+
+    # Pass the input through the model
+    output = model(input_tensor)
+
+    # The output is a tensor. If you want to convert it back to a numpy array, you can do so with the .detach().numpy() methods
+    output_array = output.detach().numpy()
+
+    # Convert the output back to the original space
+    output_original = scaler.inverse_transform(output_array)
+
+    # Clip the values to the MIDI range
+    output_clipped = np.clip(output_original, 0, 127)
+
+    # Round the values to the nearest integer and convert to int
+    output_rounded = np.round(output_clipped).astype(int)
+
+    return output_rounded[0]
 
 # Define the input array
-input_array = np.array([69, 60, 65, 64, 69])
+input = np.array([69, 60, 65, 64, 69])
 
-# Reshape the input to have 5 features
-input_array = input_array.reshape(1, -1)
+# Initialize an empty list to store the outputs
+outputs = []
+outputs = input.copy()
 
-# Scale the input to the range [0, 1] using the same scaler used for training
-input_scaled = scaler.transform(input_array)
+# Process the input and append the output to the list
+for _ in range(15):
+    print("intput: ", input)
+    output = process_input(input)
+    print("output ", output)
+    outputs = np.concatenate((outputs, output))
+    input = output
 
-# Convert the input to a PyTorch tensor
-input_tensor = torch.tensor(input_scaled, dtype=torch.float32)
+# Now, outputs is a list of 10 processed outputs
 
-# Pass the input through the model
-output = model(input_tensor)
 
-# The output is a tensor. If you want to convert it back to a numpy array, you can do so with the .detach().numpy() methods
-output_array = output.detach().numpy()
+# Create a new MIDI file
+mid = mido.MidiFile()
 
-# Convert the output back to the original space
-output_original = scaler.inverse_transform(output_array)
+# Create a new MIDI track
+track = mido.MidiTrack()
 
-# Clip the values to the MIDI range
-output_clipped = np.clip(output_original, 0, 127)
+# Add the track to the MIDI file
+mid.tracks.append(track)
 
-# Round the values to the nearest integer and convert to int
-output_rounded = np.round(output_clipped).astype(int)
+# For each note in the final output
+for note in outputs.flatten():
+    # Ensure note is an integer
+    note = int(note)
 
-handler = MidiDataHandler()
-print(handler.midi_notes_to_piano(input_array[0]))
-print(handler.midi_notes_to_piano(output_rounded[0]))
+    # Add a note_on message to the track
+    track.append(mido.Message('note_on', note=note, velocity=64, time=20))
+
+    # Add a note_off message to the track
+    track.append(mido.Message('note_off', note=note, velocity=64, time=50))
+
+# Save the MIDI file
+mid.save('output.mid')
